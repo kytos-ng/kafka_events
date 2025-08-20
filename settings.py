808d494933fe15b55949f11defba6f91af6f258b
Auwate <auwate1@gmail.com>
2025-08-20 17:26:52 -0400

    Commit: Tests

--- settings.py
+++ settings.py
@@ -47,7 +47,7 @@ KAFKA_TIMELIMIT = 5
 # FILTERING
 
 # Allowed namespaces: list[str], The names of all namespaces (in regex format) that are allowed
-ALLOWED_NAMESPACES = [r"kytos[./](.*)"]
+ALLOWED_NAMESPACES = (r"kytos[./](.*)",)
 
 # Blocked patterns: list[str], The patterns we want to explicitly block. This takes precendence
 # over ALLOWED_NAMESPACES.
